2d123a3502c9bf463504329b5f80a68e98be7392
David Cobb <github@davidcobb.net>
2020-11-25 09:06:57 +0000

    added awscli

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -6,6 +6,9 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends apt-utils dialog dnsutils httpie wget unzip curl jq
 DEBIAN_FRONTEND=dialog
 
+
+# INSTALL TERRAFORM
+# refer 
 getLatestVersion() {
 LATEST_ARR=($(wget -q -O- https://api.github.com/repos/hashicorp/terraform/releases 2> /dev/null | awk '/tag_name/ {print $2}' | cut -d '"' -f 2 | cut -d 'v' -f 2))
 for ver in "${LATEST_ARR[@]}"; do
@@ -23,3 +26,13 @@ cd ~
 wget "https://releases.hashicorp.com/terraform/"$VERSION"/terraform_"$VERSION"_linux_amd64.zip"
 unzip "terraform_"$VERSION"_linux_amd64.zip"
 sudo install terraform /usr/local/bin/
+# verify
+terraform --version
+
+# INSTALL AWSCLI
+# refer https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+# verify
+aws --version
